2e6c7074cccaf8471fee7cd33b68ca1eebed9313
Bastion01 <vlad.kornelyuk@mail.ru>
2025-10-16 12:54:04 +0300

    Menu Management Service initial draft

---
+++ menu-management-service/src/main/java/com/vwaiter/menu_management_service/MenuManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vwaiter.menu_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuManagementServiceApplication.class, args);
+	}
+
+}
---
+++ menu-management-service/src/test/java/com/vwaiter/menu_management_service/MenuManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwaiter.menu_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
