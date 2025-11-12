4fe64724e3f9f91081d68b2c8d808b54bb38e579
Bastion01 <vlad.kornelyuk@mail.ru>
2025-11-13 00:30:45 +0300

    order-management-service spring-boot application skeleton added

---
+++ order-management-service/order-management-service-application/src/main/java/com/vwaiter/OrderManagementServiceApplication.java
@@ -0,0 +1,18 @@
+package com.vwaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:database.properties")
+})
+public class OrderManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementServiceApplication.class, args);
+	}
+}
